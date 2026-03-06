abea97243eb04b9473b6fde58ae66f434965ea95
RynaMol <mac@Macs-MacBook-Pro.local>
2026-03-06 17:43:11 +0700

    gg

--- components/sections/AboutSection.tsx
+++ components/sections/AboutSection.tsx
@@ -7,7 +7,6 @@ import { useEffect, useState } from "react"
 
 export function AboutSection() {
   const [mounted, setMounted] = useState(false)
-
   useEffect(() => {
     setMounted(true)
   }, [])
