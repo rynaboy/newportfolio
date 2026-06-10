001b4a5f16de3bfc1121b030be854a4b3d69d405
rynaboy <rynaboy22@gmail.com>
2026-06-10 13:37:39 +0700

    updatemeta

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,24 @@ import type { Metadata } from 'next'
 import './globals.css' 
 
 export const metadata: Metadata = {
- 
+  title: 'MOL RYNA Portfolio',
+  description: 'Created with Next.js by MOL RYNA',
+  generator: 'Next.js',
+  openGraph: {
+    title: 'MOL RYNA Portfolio',
+    description: 'Created with Next.js by MOL RYNA',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'MOL RYNA Portfolio',
+    description: 'Created with Next.js by MOL RYNA',
+  },
+  icons: {
+    icon: '/molryna.ico',
+    shortcut: '/molryna.ico',
+    apple: '/molryna.ico',
+  },
 }
  
 export default function RootLayout({
