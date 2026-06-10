84e5750228ac9feb94092be09a656bdaaf7ef710
rynaboy <rynaboy22@gmail.com>
2026-06-10 13:49:22 +0700

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,13 @@ export const metadata: Metadata = {
     title: 'MOL RYNA Manager | Developer',
     description: 'Full Stack Developer, IT Manager, Project Manager, UX/UI Designer, Mobile Developer, and IT Support with over 5 years of experience. Skilled in Laravel, Next.js, React Native, Flutter, TypeScript, Cpanel, PostgreSQL, Git, RawSQL, and Sqlite.',
     type: 'website',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
   },
   twitter: {
     card: 'summary_large_image',
